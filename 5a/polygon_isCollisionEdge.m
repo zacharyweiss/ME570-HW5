0bb3d8aa39c3fde2effb97121cf2541911c704b2
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-05 01:17:30 -0500

    Output type fix

--- 5a/polygon_isCollisionEdge.m
+++ 5a/polygon_isCollisionEdge.m
@@ -3,9 +3,9 @@
 %whether an edge is in collision with a polygon.
 function [flagIsCollision]=polygon_isCollisionEdge(edge,vertices)
    nPairs = size(vertices,2)-1;
-   flagIsCollision = zeros(1,nPairs);
+   flagIsCollision = false;
 
    for iEdge = 1:nPairs
-        flagIsCollision(iEdge) = edge_isCollision(edge,vertices(:,iEdge:iEdge+1));
+        flagIsCollision = any([edge_isCollision(edge,vertices(:,iEdge:iEdge+1) flagIsCollision]);
    end
 end
